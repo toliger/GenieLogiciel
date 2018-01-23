4a1c35c932abb4664683b03eba88d7b4b1124c9c
wiztigers <regis.witz@free.fr>
2018-01-23 17:31:04 +0100

    création et publication automatique d'un pdf à coté du html.

---
+++ build-pdf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+exportdir=$1
+projectname=$2
+
+# we have to wait 'a few seconds' for current version of gh-pages to show up 
+# ... kind of ugly tho'
+sleep 30s
+# get the latest version of wkhtmltopdf and untar it
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+
+echo "Generating PDF ..."
+./wkhtmltox/bin/wkhtmltopdf --enable-internal-links --enable-external-links https://wiztigers.github.io/${projectname}/ ${exportdir}/${projectname}.pdf
+
